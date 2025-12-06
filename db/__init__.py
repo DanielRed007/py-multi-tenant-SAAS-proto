764957d2541f770f32e77935c4fa19a5dc2226d0
Danielred007 <23222122+DanielRed007@users.noreply.github.com>
2025-12-06 02:49:18 -0300

    Undesrtanding the __init__.py module system in python, this si great

---
+++ db/__init__.py
@@ -0,0 +1,2 @@
+# db/__init__.py
+from .db import get_all, get_by_id, create
---
+++ models/__init__.py
@@ -0,0 +1,2 @@
+# models/__init__.py   ← THIS LINE IS CRUCIAL
+from .user import User, UserCreate   # ← expose them here!
